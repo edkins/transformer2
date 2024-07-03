2548ecf3441a9a7203958bc594339a8dc454dd7f
Giles Edkins <edkins@gmail.com>
2024-07-03 13:58:08 -0400

    Add tokenize stage

--- data-tools/src/main.rs
+++ data-tools/src/main.rs
@@ -1,3 +1,6 @@
+use std::io::{BufRead, Write};
+
+use clap::{Parser, Subcommand};
 use word_counter::WordCounter;
 
 mod bpe;
@@ -9,9 +12,26 @@ mod tokenize;
 mod word_counter;
 
 const MAX_ARTICLES_TO_PROCESS: usize = 5000;
+const CORPUS_FILENAME: &str = "../enwiki-latest-pages-articles-multistream.xml.bz2";
+const DICTIONARY_FILENAME: &str = "dictionary.txt";
+const TOKENIZED_FILE: &str = "tokenized.bin";
+
+#[derive(Parser)]
+struct Args {
+    #[clap(subcommand)]
+    subcmd: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Dictionary {},
+    Tokenize {},
+}
 
 fn main() {
-    let filename = "../enwiki-latest-pages-articles-multistream.xml.bz2";
+    let cli = Args::parse();
+
+    let filename = CORPUS_FILENAME;
     let file_size = std::fs::metadata(filename).unwrap().len();
     let file = std::fs::File::open(filename).unwrap();
     let progress_file = progress_reader::ProgressReader::new(file, file_size);
@@ -19,11 +39,34 @@ fn main() {
     let decomp = bzip2::read::MultiBzDecoder::new(bfile);
     let bdecomp = std::io::BufReader::new(decomp);
 
+    match cli.subcmd {
+        Command::Dictionary {} => dictionary(bdecomp),
+        Command::Tokenize {} => tokenize(bdecomp),
+    }
+}
+
+fn dictionary(bdecomp: impl BufRead) {
     process_xml::ArticleReader::new(bdecomp)
         .filter_map(process_wikitext::strip_wikitext)
         .take(MAX_ARTICLES_TO_PROCESS)
         .flat_map(split_words::split_words)
         .collect::<WordCounter>()
         .into_bpe()
-        .write_to_file("dictionary.txt");
+        .write_to_file(DICTIONARY_FILENAME);
+}
+
+fn tokenize(bdecomp: impl BufRead) {
+    let mut tokenizer = tokenize::Tokenizer::from_file(DICTIONARY_FILENAME);
+
+    let vector = process_xml::ArticleReader::new(bdecomp)
+        .filter_map(process_wikitext::strip_wikitext)
+        .take(MAX_ARTICLES_TO_PROCESS)
+        .flat_map(split_words::split_words)
+        .flat_map(|word| tokenizer.tokenize_word_to_bytes(word).to_vec())
+        .collect::<Vec<_>>();
+
+    println!("Tokenized {} bytes", vector.len());
+
+    let mut file = std::fs::File::create(TOKENIZED_FILE).unwrap();
+    file.write_all(&vector).unwrap();
 }
--- data-tools/src/tokenize.rs
+++ data-tools/src/tokenize.rs
@@ -1,4 +1,8 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, io::{BufRead, BufReader}};
+
+use base64::{prelude::BASE64_STANDARD, Engine};
+
+use crate::split_words::Word;
 
 pub type Token = u16;
 
@@ -7,6 +11,15 @@ pub struct Tokenizer {
 }
 
 impl Tokenizer {
+    pub fn from_file(filename: &str) -> Self {
+        let dictionary = std::fs::File::open(filename).unwrap();
+        let dictionary = BufReader::new(dictionary)
+            .lines()
+            .map(|line| BASE64_STANDARD.decode(line.unwrap()).unwrap())
+            .collect();
+        Tokenizer::new(dictionary)
+    }
+
     pub fn new(dictionary: Vec<Vec<u8>>) -> Self {
         let mut cache = HashMap::new();
         for (i, word) in dictionary.into_iter().enumerate() {
@@ -30,7 +43,7 @@ impl Tokenizer {
             if i == 0 {
                 panic!("No token found for {:?}", text);
             }
-            if let Some(token) = self.single_token(text) {
+            if let Some(token) = self.single_token(&text[..i]) {
                 return (token, &text[i..]);
             }
             i -= 1;
@@ -48,7 +61,19 @@ impl Tokenizer {
         tokens
     }
 
-    pub fn tokenize_word<'a>(&'a mut self, word: &[u8]) -> &'a [Token] {
+    pub fn tokenize_word_to_bytes(&mut self, word: Word) -> Vec<u8> {
+        let tokens = self.tokenize_word(word);
+        tokens
+            .iter()
+            .flat_map(|x|x.to_le_bytes())
+            .collect()
+    }
+
+    pub fn tokenize_word(&mut self, word: Word) -> &[Token] {
+        self.tokenize_word_from_bytes(word.as_bytes())
+    }
+
+    pub fn tokenize_word_from_bytes<'a>(&'a mut self, word: &[u8]) -> &'a [Token] {
         if !self.cache.contains_key(word) {
             let tokens = self.split_into_tokens(word);
             self.cache.insert(word.to_vec(), tokens);
