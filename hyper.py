798f018768be5323a87b46c2f53a76586fa9d698
Giles Edkins <edkins@gmail.com>
2024-07-15 17:34:29 -0400

    More initial magnitude experiments

--- hyper.py
+++ hyper.py
@@ -2,9 +2,9 @@ import argparse
 import os
 import subprocess
 
-def run(input_file: str, n_layer=1, n_head=4, d_model=128, d_k=4, d_hidden=128, time_s=300, mag=0.05):
+def run(input_file: str, n_layer=1, n_head=4, d_model=128, d_k=4, d_hidden=128, time_s=300, mag=0.1):
     output_file = f'data/l{n_layer}_h{n_head}_d{d_model}_k{d_k}_h{d_hidden}'
-    if mag != 0.05:
+    if mag != 0.05 or True:
         output_file += f'_m{mag}'
     output_file += '.json'
     if os.path.exists(output_file):
@@ -17,11 +17,10 @@ def main():
     parser.add_argument('input_file', type=str)
     os.makedirs('data', exist_ok=True)
     inp = parser.parse_args().input_file
-    #run(inp)
-    run(inp, mag=0.5)
-    run(inp, mag=0.005)
-    run(inp, mag=0.1)
-    run(inp, mag=0.03)
+    run(inp, n_layer=2, mag=0.03)
+    run(inp, n_layer=2, mag=0.05)
+    run(inp, n_layer=2, mag=0.1)
+    run(inp, n_layer=2, mag=0.2)
 
 if __name__ == '__main__':
     main()
